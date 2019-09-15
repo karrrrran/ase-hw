0cc0c6451113821fec89916ccf61d91fafe1196c
Darpan Dodiya <darpandodiya@gmail.com>
2019-09-14 22:42:33 -0400

    #1. Fixed error for output formatting

--- hw/2/hw2.py
+++ hw/2/hw2.py
@@ -1,127 +1,190 @@
 import re
 import statistics
+import zipfile
 from operator import itemgetter
-import json 
+import jsonpickle
+import json
+import sys
+
+
+class SYMBOLS:
+    sep = ","
+    num = "$"
+    less = "<"
+    more = ">"
+    skip = "?"
+    doomed = r'([\n\t\r ]|#.*)'
+
 
 def compiler(x):
     "return something that can compile strings of type x"
-    try: int(x); return  int 
-    except:
-        try: float(x); return  float
-        except ValueError: return str
+    """return something that can compile strings"""
+    def num(z):
+        f = float(z)
+        i = int(f)
+        return i if i == f else f
+
+    for c in [SYMBOLS.num, SYMBOLS.less, SYMBOLS.more]:
+        if c in x:
+            return num
+
 
 def string(s):
     "read lines from a string"
     for line in s.splitlines(): yield line
+
 
 def file(fname):
     "read lines from a fie"
     with open(fname) as fs:
         for line in fs: yield line
 
-def zipped(archive,fname):
+
+def zipped(archive, fname):
     "read lines from a zipped file"
     with zipfile.ZipFile(archive) as z:
         with z.open(fname) as f:
             for line in f: yield line
 
-def rows(src, sep= ",", doomed = r'([\n\t\r ]|#.*)'):
+
+def rows(src, sep=SYMBOLS.sep, doomed=SYMBOLS.doomed):
     "convert lines into lists, killing whitespace and comments"
-    for line in src:
+    linesize = None
+    for n, line in enumerate(src):
         line = line.strip()
         line = re.sub(doomed, '', line)
-        #Skip blanks
         if line:
-            yield line.split(sep)
+            line = line.split(sep)
+            # update the linesize for first time
+            if linesize is None:
+                linesize = len(line)
 
-def cells(src,valid_length):
+            # skip line if it doesn't match size
+            if len(line) == linesize:
+                yield line
+            else:
+                print("E> skipping line %s" % n)
+
+
+def cols(src):
+    "skip columns whose name contains '?'"
+    valid_cols = None
+    for cells in src:
+        if valid_cols is None:  # Do this just for the first row
+            valid_cols = [n for n, cell in enumerate(cells) if not SYMBOLS.skip in cell]
+        yield [cells[n] for n in valid_cols]
+
+
+def cells(src):
     "convert strings into their right types"
-    oks = None
-    for n,cells in enumerate(src):
-        #If total values are less than total number of column 
-        if len(cells) != valid_length:
-            print ("E> skipping line due to insufficient data points") 
-            continue
-        # Skipping '?' valued data points
-        oks = [compiler(cell) for cell in cells]
-        yield [f(cell) for f,cell in zip(oks,cells)]
+    one = next(src)
+    fs = [None] * len(one)  # [None, None, None, None]
+    yield one  # the first line
 
-def fromString(s):
-    "putting it all together"
-    all_rows = rows(string(s))
-    column_names = next(all_rows)
-    valid_columns = [x for x in range(len(column_names)) if '?' not in column_names[x]] 
-    yield (itemgetter(*valid_columns)(column_names))
-    for lst in cells(all_rows,len(column_names)):
-        yield list(itemgetter(*valid_columns)(lst))
+    def ready(n, cell):
+        if cell == SYMBOLS.skip:
+            return cell  # skip over '?'
+        fs[n] = fs[n] or compiler(one[n])  # ensure column 'n' compiles
+        return fs[n](cell)  # compile column 'n'
+
+    for _, cells in enumerate(src):
+        yield [ready(n, cell) for n, cell in enumerate(cells)]
 
 
-class Row:
+def fromString(input_str):
+    "read lines fro string"
+    for line in input_str.splitlines():
+        yield line
+
+
+class MyID:
+    oid = 0
+
+    def generate_oid(self):
+        MyID.oid += 1
+        self.oid = MyID.oid
+        return self.oid
+
+
+class Row(MyID):
     "Row class describing each row in data"
-    def __init__(self, cells, cooked = [], dom = 0):
+
+    def __init__(self, cells, cooked=[], dom=0):
+        self.generate_oid()
         self.cells = cells
         self.cooked = cooked
         self.dom = dom
 
 
-class Col:
+class Col(MyID):
     "Col class for each column in data"
-    def __init__(self,column_name, position):
+
+    def __init__(self, column_name, position):
+        self.generate_oid()
         self.column_name = column_name
         self.position = position
 
 
 class Num(Col):
     "Num class as a subclass of Col"
+
     def __init__(self, column_name, position):
-        super().__init__(column_name,position)
-        self.mean = 0
-        self.sd = 0
+        super().__init__(column_name, position)
+        self.n = 0
+        self.mu = 0     # mean
+        self.m2 = 0     # square diff
+        self.lo = 10 ** 32
+        self.hi = -1 * 10 ** 32
         self.all_values = []
+        self.sd = 0
 
     def add_new_value(self, number):
+        "Add new value to the list and update the paramaters"
         self.all_values.append(number)
-        self.update_mean_and_sd()
+        if number < self.lo:
+            self.lo = number
+        if number > self.hi:
+            self.hi = number
 
-    def update_mean_and_sd(self):
-        self.mean = statistics.mean(self.all_values) if len(self.all_values) > 0 else 0
-        self.sd = statistics.stdev(self.all_values) if len(self.all_values) > 1 else 0
+        self.n += 1
+        d = number - self.mu
+        self.mu += d / self.n
+        self.m2 += d * (number - self.mu)
+        self.sd = 0 if self.n < 2 else (self.m2 / (self.n - 1 + 10 ** -32)) ** 0.5
 
     def delete_from_behind(self):
-        self.all_values.pop()
-        self.update_mean_and_sd()
+        "Remove a value from behind the list"
+        number = self.all_values.pop()
+        if self.n < 2:
+            self.n, self.mu, self.m2 = 0, 0, 0
+        else:
+            self.n -= 1
+            d = number - self.mu
+            self.mu -= d / self.n
+            self.m2 -= d * (number - self.mu)
+            self.sd = 0 if self.n < 2 else (self.m2 / (self.n - 1 + 10 ** -32)) ** 0.5
 
 
 class Tbl:
     "Table class for driving the tables comprising of Rows and Cols"
+
     def __init__(self):
-        self.rows = list() #Will hold Row objects for each row
-        self.cols = list() #Will hold Num objects for each column
+        self.rows = list()  # Will hold Row objects for each row
+        self.cols = list()  # Will hold Num objects for each column
+        self.oid = 0
 
     def dump(self):
-        print ("Table Object")
-        print ("Columns--")
-        for idx,col in enumerate(self.cols):
-            print("\t --{0}".format(idx))
-            print ("\t \t Mean: {0}".format(round(col.mean,2)))
-            print ("\t \t SD: {0}".format(round(col.sd,2)))
-            print ("\t \t Column-name: {0}".format(col.column_name))
-        print ("\n")
-        print ("Rows--")
-        for idx,row in enumerate(self.rows):
-            print("\t --{0}".format(idx))
-            print ("\t \t cells")
-            for j,cell in enumerate(row.cells):
-                print ("\t \t {0}: {1}".format(j,cell))
-            print ("\t \t cooked: {0}".format(row.cooked))
-            print ("\t \t dom: {0}".format(row.dom))
+        # replaced manual printing with JSON output
+        print("Table Object")
+        # Single line output to JSON
+        print(json.dumps(json.loads(jsonpickle.encode(self)), indent=4, sort_keys=True))
 
-    
-    def read(self, s):
-        for idx, row in enumerate(fromString(s)):
+    def read(self, inputfile):
+        for idx, row in enumerate(cells(cols(rows(fromString(inputfile))))):
+            print(row)
             if idx == 0:
                 # Column names are here
-                self.cols = [Num(x,i) for i,x in enumerate(row)]
+                self.cols = [Num(x, i) for i, x in enumerate(row)]
             else:
                 for i in range(len(self.cols)):
                     self.cols[i].add_new_value(row[i])
@@ -129,7 +192,7 @@ class Tbl:
 
 
 if __name__ == "__main__":
-    s="""
+    s = """
         $cloudCover, $temp, ?$humid, <wind,  $playHours
         100,        68,    80,    0,    3   # comments
         0,          85,    85,    0,    0
@@ -141,12 +204,30 @@ if __name__ == "__main__":
         70,         64,    65,    15,   5
         0,          72,    95,    0,    0
         0,          69,    70,    0,    4
-        50,          75,    80,    0,    60
+        0,          75,    80,    0,    0
         0,          75,    70,    18,   4
         60,         72,
         40,         81,    75,    0,    2
         100,        71,    91,    15,   0
   """
+
+    s2 = """
+    $cloudCover, $temp, $humid, $wind,  $playHours
+  100,         68,    80,     0,      3   
+  0,           85,    85,     0,      0
+  0,           80,    90,     10,     0
+  60,          83,    86,     0,      4
+  100,         70,    96,     0,      3
+  100,         65,    70,     20,     0
+  70,          64,    65,     15,     5
+  0,           72,    95,     0,      0
+  0,           69,    70,     0,      4
+  80,          75,    80,     0,      3
+  0,           75,    70,     18,     4
+  60,          72,    83,     15,     5
+  40,          81,    75,     0,      2
+  100,         71,    91,     15,     0
+    """
     table = Tbl()
     table.read(s)
     table.dump()
