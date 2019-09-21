470339b86bac839566f997bac428d9cdaed086e3
rvsingh31 <ravinder.singh3196@gmail.com>
2019-09-21 12:18:37 -0400

    ZeroR completed

--- hw/4/zeroR.py
+++ hw/4/zeroR.py
@@ -10,6 +10,7 @@ class ZeroR:
     def __init__(self):
         self.tbl = Tbl()
         self.abcd = Abcd()
+        self.wait = 2
 
     def zeroRTrain(self, file_name):
         file_contents = cells(cols(rows(file(file_name))))
@@ -17,8 +18,28 @@ class ZeroR:
             if idx == 0:
                 self.tbl.addCol(row)
             else:
+                if idx > self.wait:
+                    #Classify the data point
+                    count = 0
+                    got = self.classify() #predicted label
+                    want = row[self.tbl.col_info["goals"][0]] #true label
+                    self.abcd.abcd1(want,got)
                 self.tbl.addRow(row)
-    
 
-z = ZeroR()
-z.zeroRTrain("weathernon.csv")
+
+    def classify(self):
+        return self.tbl.cols[self.tbl.col_info["goals"][0]].mode
+
+
+if __name__ == "__main__":
+
+    z1 = ZeroR()
+    print ("#--- zerorok ---------------------")
+    print ("weathernon")
+    z1.zeroRTrain("weathernon.csv")
+    z1.abcd.abcd_report()
+    print ("\n")
+    z2 = ZeroR()
+    print ("diabetes")
+    z2.zeroRTrain("diabetes.csv")
+    z2.abcd.abcd_report()
